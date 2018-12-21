4a91bf63322fd9d8833d9148bc1fc59a69f0ed5a
IGoldin <squl2squl@hotmail.com>
2018-12-20 23:56:49 -0700

    Update telephone-number-validator.js

--- telephone-number-validator/telephone-number-validator.js
+++ telephone-number-validator/telephone-number-validator.js
@@ -1,9 +1,7 @@
-//Challenge description that outlines a problem this code solves is available here LINK
-
 //make life easier
 let log = console.log;
 
-//telephoneCheck function is a phone format validator
+//telephoneCheck function is a phone number format validator
 //it accepts a string and returns a boolean
 function telephoneCheck(str) {
    let regex = /^(1\s?)?(\(\d{3}\)|\d{3})[\s\-]?\d{3}[\s\-]?\d{4}$/; 
